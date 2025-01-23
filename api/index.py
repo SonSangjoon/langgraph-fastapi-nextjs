a63ad7cedbe87d078a65b35bafe71600f5dd667c
onshore <onshorecoding9@gmail.com>
2025-01-24 04:54:21 +0900

    feat: add langgraph stream with remote graph

--- api/index.py
+++ api/index.py
@@ -1,153 +1,100 @@
 import os
 import json
 from typing import List
-from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+from langchain_core.messages import HumanMessage
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from fastapi import FastAPI, Query
 from fastapi.responses import StreamingResponse
 from openai import OpenAI
+from langgraph.pregel.remote import RemoteGraph
 from .utils.prompt import ClientMessage, convert_to_openai_messages
 from .utils.tools import get_current_weather
 
 
-load_dotenv(".env.local")
+from langgraph_sdk import get_client, get_sync_client
+
+load_dotenv()
 
 app = FastAPI()
 
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
+
+# Connect via remote graph
 
 
 class Request(BaseModel):
     messages: List[ClientMessage]
 
 
-available_tools = {
-    "get_current_weather": get_current_weather,
-}
-
-def do_stream(messages: List[ChatCompletionMessageParam]):
-    stream = client.chat.completions.create(
-        messages=messages,
-        model="gpt-4o",
-        stream=True,
-        tools=[{
-            "type": "function",
-            "function": {
-                "name": "get_current_weather",
-                "description": "Get the current weather at a location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "latitude": {
-                            "type": "number",
-                            "description": "The latitude of the location",
-                        },
-                        "longitude": {
-                            "type": "number",
-                            "description": "The longitude of the location",
-                        },
-                    },
-                    "required": ["latitude", "longitude"],
-                },
-            },
-        }]
-    )
-
-    return stream
-
-def stream_text(messages: List[ChatCompletionMessageParam], protocol: str = 'data'):
+async def stream_text(messages):
+    graph_name = "task_maistro" 
+    url = "http://localhost:8123"
+    graph_name = "task_maistro"
+    client = get_client(url=url)
+    sync_client = get_sync_client(url=url)
+    remote_graph = RemoteGraph(graph_name, client=client, sync_client=sync_client)
+    thread = await client.threads.create()
+    config = {"configurable": {"user_id": "Test-Double-Texting"}}
     draft_tool_calls = []
     draft_tool_calls_index = -1
 
-    stream = client.chat.completions.create(
-        messages=messages,
-        model="gpt-4o",
-        stream=True,
-        tools=[{
-            "type": "function",
-            "function": {
-                "name": "get_current_weather",
-                "description": "Get the current weather at a location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "latitude": {
-                            "type": "number",
-                            "description": "The latitude of the location",
-                        },
-                        "longitude": {
-                            "type": "number",
-                            "description": "The longitude of the location",
-                        },
-                    },
-                    "required": ["latitude", "longitude"],
-                },
-            },
-        }]
-    )
-
-    for chunk in stream:
-        for choice in chunk.choices:
-            if choice.finish_reason == "stop":
+    try:
+        async for chunk in remote_graph.astream(
+            input={"messages": messages}, 
+            config=config,
+            stream_mode=["messages"]
+        ):
+            finish_reason = chunk.data[0]['response_metadata'].get('finish_reason')
+            if finish_reason == "stop":
                 continue
 
-            elif choice.finish_reason == "tool_calls":
-                for tool_call in draft_tool_calls:
-                    yield '9:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
-                        id=tool_call["id"],
-                        name=tool_call["name"],
-                        args=tool_call["arguments"])
+
+            elif finish_reason == "tool_calls":
 
                 for tool_call in draft_tool_calls:
-                    tool_result = available_tools[tool_call["name"]](
-                        **json.loads(tool_call["arguments"]))
-
-                    yield 'a:{{"toolCallId":"{id}","toolName":"{name}","args":{args},"result":{result}}}\n'.format(
-                        id=tool_call["id"],
-                        name=tool_call["name"],
-                        args=tool_call["arguments"],
-                        result=json.dumps(tool_result))
-
-            elif choice.delta.tool_calls:
-                for tool_call in choice.delta.tool_calls:
-                    id = tool_call.id
-                    name = tool_call.function.name
-                    arguments = tool_call.function.arguments
-
-                    if (id is not None):
-                        draft_tool_calls_index += 1
-                        draft_tool_calls.append(
-                            {"id": id, "name": name, "arguments": ""})
-
+                    if tool_call["name"]:
+                        print("tool_call end")
+                        yield 'a:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
+                            id=tool_call["id"],
+                            name=tool_call["name"],
+                            args=tool_call["arguments"])
                     else:
-                        draft_tool_calls[draft_tool_calls_index]["arguments"] += arguments
+                        print("tool_call terminate")  
+                        yield '9:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
+                            id=tool_call["id"],
+                            name=tool_call["name"],
+                            args=tool_call["arguments"])
+            # elif chunk.data[0]['tool_call_chunks']:
+            #     tool_call_chunks = chunk.data[0]['tool_call_chunks']
+            #     if tool_call_chunks:
+            #         for tool_call_chunk in tool_call_chunks:
+            #             if tool_call_chunk["name"]:
+            #                 print("tool_call_chunks start")
 
+            #                 yield 'b:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
+            #                     id=tool_call_chunk["id"],
+            #                     name=tool_call_chunk["name"],
+            #                     args=tool_call_chunk["args"],
+            #                 )
+            #             else:
+            #                 print("tool_call_chunks processing")
+            #                 yield 'c:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
+            #                     id=tool_call_chunk["id"],
+            #                     name=tool_call_chunk["name"],
+            #                     args=tool_call_chunk["args"],
+            #                 )
+            #     else:
+            #         print("tool_call_chunks missing")
             else:
-                yield '0:{text}\n'.format(text=json.dumps(choice.delta.content))
+                yield '0:{text}\n'.format(text=json.dumps(chunk.data[0]['content']))
 
-        if chunk.choices == []:
-            usage = chunk.usage
-            prompt_tokens = usage.prompt_tokens
-            completion_tokens = usage.completion_tokens
-
-            yield 'e:{{"finishReason":"{reason}","usage":{{"promptTokens":{prompt},"completionTokens":{completion}}},"isContinued":false}}\n'.format(
-                reason="tool-calls" if len(
-                    draft_tool_calls) > 0 else "stop",
-                prompt=prompt_tokens,
-                completion=completion_tokens
-            )
-
-
+    except Exception as e:
+        yield f"data: {str(e)}\n\n"
 
 
 @app.post("/api/chat")
 async def handle_chat_data(request: Request, protocol: str = Query('data')):
-    messages = request.messages
-    openai_messages = convert_to_openai_messages(messages)
-
-    response = StreamingResponse(stream_text(openai_messages, protocol))
-    response.headers['x-vercel-ai-data-stream'] = 'v1'
+    response = StreamingResponse(stream_text(request.messages))
+    response.headers['x-vercel-ai-data-stream'] = 'v2'
     return response
+
